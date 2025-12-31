c8aaf5b5ecb70a81fd17b65cee88075548eebb95
Danishwara Pracheta <danishwara81@gmail.com>
2025-12-31 08:11:46 +0800

    updated web title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Era Solution') }}</title>
+        <title>{{ config('app.name', 'Company Name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
